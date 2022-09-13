5b5f3d4a233e9a571884f6088e39b511e3efde40
Macias Romero <a00573134@itesm.mx>
2022-09-13 15:51:56 -0500

    Login

---
+++ ig2/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { Milogin } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Milogin
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ig2/src/app/components/login/login.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html'
+})
+export class Milogin{
+  constructor() {
+    console.log("Sí salió")
+  }
+}
