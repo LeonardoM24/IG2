6976eb8174e973b0c55c9987fd8d30dfe2913233
abiel-lozano <73564559+abiel-lozano@users.noreply.github.com>
2022-09-14 16:24:56 -0500

    Multiple pages on site

--- ig2/src/app/app.module.ts
+++ ig2/src/app/app.module.ts
@@ -4,20 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { Milogin } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'login', component: Milogin },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     Milogin,
-    HomepageComponent
+    HomepageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
