6c11539809cf9ae2506cc993bbef7fd4729365a6
Macias Romero <a00573134@itesm.mx>
2022-09-14 15:19:59 -0500

    Sin necesidad de un APP2

--- ig2/src/app/app-routing.module.ts
+++ ig2/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from '../app/components/homepage/homepage.component'
+import { Milogin } from '../app/components/login/login.component'
 
 const routes: Routes = [
-  {path:'Homepage', component: HomepageComponent}
+  {
+    path:'Homepage', component: HomepageComponent,
+
+  },
+  {
+    path: '', component: Milogin
+  }
 ];
 
 @NgModule({
